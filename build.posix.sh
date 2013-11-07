6bdd042f46ce0d0a74565c87366126112ad646a9
Richard Varbelow <r.varbelow@gmail.com>
2013-11-07 13:55:00 +0100

    further adaptations build script
    added three more paths to PKG_CONFIG_PATH

--- build.posix.sh
+++ build.posix.sh
@@ -5,7 +5,7 @@ GAPATH=`pwd`
 popd > /dev/null
 
 export GADEPS=$GAPATH/deps.posix
-export PKG_CONFIG_PATH=$GADEPS/lib/pkgconfig:/opt/local/lib/pkgconfig:/usr/lib/i386-linux-gnu/pkgconfig/:/usr/lib/pkgconfig
+export PKG_CONFIG_PATH=$GADEPS/lib/pkgconfig:/opt/local/lib/pkgconfig:/usr/lib/i386-linux-gnu/pkgconfig/:/usr/lib/pkgconfig:/usr/local/Cellar/ffmpeg/1.2.4/lib/pkgconfig/:/usr/local/Cellar/sdl2/2.0.0/lib/pkgconfig/:/usr/local/Cellar/sdl2_ttf/2.0.12/lib/pkgconfig/
 export PATH=$GADEPS/bin:$PATH
 
 cd $GAPATH
